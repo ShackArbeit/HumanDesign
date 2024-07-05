0a91883b513ea598cb5ec431779fdf3c7fcbd7f0
Shack <g0972222165@gmail.com>
2024-07-05 11:14:23 +0800

    Add New URL

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,7 @@ app.use(sessionMiddleware)
 app.use(cors());
 app.use(express.json())
 const corsOptions = {
-  origin: 'http://localhost:5173', 
+  origin: ' https://shackarbeit.github.io/HumanDesign/', 
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
